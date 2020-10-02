a86d0eb7d41b2f354a630d5edaa67d7d341ec32c
knnorbi <kluka.norbert@gmail.com>
2020-10-02 14:29:46 +0200

    Initial commit

---
+++ Teglalap_20_21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teglalap_20_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mekkorák a téglalap oldalai?");
+            Console.WriteLine("a oldal?");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("b oldal?");
+            int b = int.Parse(Console.ReadLine());
+
+            int kerulet = 2 * (a + b);
+            int terulet = a * b;
+
+            Console.WriteLine("Kerület: " + kerulet);
+            Console.WriteLine("Terület: " + terulet);
+
+            Console.ReadLine();
+        }
+    }
+}
